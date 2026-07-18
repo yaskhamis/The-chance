1baceccea01157c25072c8eaa799dec7aa795f3f
yaskhamis <202502872@student.polytechnic.bh>
2026-07-18 19:39:56 +0300

    Update fishandchips.go

--- Level 3/fishandchips/fishandchips.go
+++ Level 3/fishandchips/fishandchips.go
@@ -1,17 +1,20 @@
-package student
+package piscine
 
 func FishAndChips(n int) string {
-	if n < 0 || !(n%2 == 0 || n%3 == 0) {
-		return "error"
-	}
-	if n%2 == 0 && n%3 == 0 {
+	div2 := n%2 == 0
+	div3 := n%3 == 0
+
+	if div2 && div3 {
 		return "fish and chips"
 	}
-	if n%2 == 0 {
+	if div2 {
 		return "fish"
 	}
-	if n%3 == 0 {
+	if div3 {
 		return "chips"
 	}
-	return "error"
+	if n < 0 {
+		return "error: number is negative"
+	}
+	return "error: non divisible"
 }
